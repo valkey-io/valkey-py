2cde990407411e35bb4f048f81d44e7af65ab4cd
ahmedsobeh <ahmed.sobeh@aiven.io>
2024-06-28 16:37:08 +0200

    created url_parser and removed duplication
    Signed-off-by: ahmedsobeh <ahmed.sobeh@aiven.io>

--- valkey/_parsers/__init__.py
+++ valkey/_parsers/__init__.py
@@ -4,7 +4,7 @@ from .encoders import Encoder
 from .hiredis import _AsyncHiredisParser, _HiredisParser
 from .resp2 import _AsyncRESP2Parser, _RESP2Parser
 from .resp3 import _AsyncRESP3Parser, _RESP3Parser
-from url_parser import parse_url
+from .url_parser import parse_url
 __all__ = [
     "AsyncCommandsParser",
     "_AsyncHiredisParser",
