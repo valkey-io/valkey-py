redis/redis-py

Add missing type hints for `retry.py` (#3250)
Add missing type hints in the retry.py file and related tests.

tests/test_retry.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
 
 import pytest
-from redis.backoff import ExponentialBackoff, NoBackoff
+from redis.backoff import AbstractBackoff, ExponentialBackoff, NoBackoff
 from redis.client import Redis
 from redis.connection import Connection, UnixDomainSocketConnection
 from redis.exceptions import (
@@ -15,7 +15,7 @@ from redis.retry import Retry
 from .conftest import _get_client
 
 
-class BackoffMock:
+class BackoffMock(AbstractBackoff):
     def __init__(self):
         self.reset_calls = 0
         self.calls = 0

valkey-io/valkey-py

Co-authored-by: Salvatore Mesoraca <salvatore.mesoraca@aiven.io>
Signed-off-by: Salvatore Mesoraca <salvatore.mesoraca@aiven.io>

tests/test_retry.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
 
 import pytest
-from valkey.backoff import ExponentialBackoff, NoBackoff
+from valkey.backoff import AbstractBackoff, ExponentialBackoff, NoBackoff
 from valkey.client import Valkey
 from valkey.connection import Connection, UnixDomainSocketConnection
 from valkey.exceptions import (
@@ -15,7 +15,7 @@ from valkey.retry import Retry
 from .conftest import _get_client
 
 
-class BackoffMock:
+class BackoffMock(AbstractBackoff):
     def __init__(self):
         self.reset_calls = 0
         self.calls = 0