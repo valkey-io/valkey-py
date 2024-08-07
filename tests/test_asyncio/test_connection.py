redis/redis-py

Format connection errors in the same way everywhere (#3305)
Connection errors are formatted in four places, sync and async, network
socket and unix socket. Each place has some small differences compared
to the others, while they could be, and should be, formatted in an
uniform way. Factor out the logic in a helper method and call that
method in all four places. Arguably we lose some specificity, e.g. the
words "unix socket" won't be there anymore, but it is more valuable to not
have code duplication.

tests/test_asyncio/test_connection.py
@@ -12,7 +12,12 @@ from redis._parsers import (
     _AsyncRESPBase,
 )
 from redis.asyncio import ConnectionPool, Redis
-from redis.asyncio.connection import Connection, UnixDomainSocketConnection, parse_url
+from redis.asyncio.connection import (
+    Connection,
+    SSLConnection,
+    UnixDomainSocketConnection,
+    parse_url,
+)
 from redis.asyncio.retry import Retry
 from redis.backoff import NoBackoff
 from redis.exceptions import ConnectionError, InvalidResponse, TimeoutError
@@ -494,18 +499,50 @@ async def test_connection_garbage_collection(request):
 
 
 @pytest.mark.parametrize(
-    "error, expected_message",
+    "conn, error, expected_message",
     [
-        (OSError(), "Error connecting to localhost:6379. Connection reset by peer"),
-        (OSError(12), "Error connecting to localhost:6379. 12."),
+        (SSLConnection(), OSError(), "Error connecting to localhost:6379."),
+        (SSLConnection(), OSError(12), "Error 12 connecting to localhost:6379."),
         (
+            SSLConnection(),
             OSError(12, "Some Error"),
-            "Error 12 connecting to localhost:6379. [Errno 12] Some Error.",
+            "Error 12 connecting to localhost:6379. Some Error.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/redis.sock"),
+            OSError(),
+            "Error connecting to unix:///tmp/redis.sock.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/redis.sock"),
+            OSError(12),
+            "Error 12 connecting to unix:///tmp/redis.sock.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/redis.sock"),
+            OSError(12, "Some Error"),
+            "Error 12 connecting to unix:///tmp/redis.sock. Some Error.",
         ),
     ],
 )
-async def test_connect_error_message(error, expected_message):
+async def test_format_error_message(conn, error, expected_message):
     """Test that the _error_message function formats errors correctly"""
-    conn = Connection()
     error_message = conn._error_message(error)
     assert error_message == expected_message
+
+
+async def test_network_connection_failure():
+    with pytest.raises(ConnectionError) as e:
+        redis = Redis(host="127.0.0.1", port=9999)
+        await redis.set("a", "b")
+    assert str(e.value).startswith("Error 111 connecting to 127.0.0.1:9999. Connect")
+
+
+async def test_unix_socket_connection_failure():
+    with pytest.raises(ConnectionError) as e:
+        redis = Redis(unix_socket_path="unix:///tmp/a.sock")
+        await redis.set("a", "b")
+    assert (
+        str(e.value)
+        == "Error 2 connecting to unix:///tmp/a.sock. No such file or directory."
+    )

tests/test_connection.py
@@ -296,3 +296,53 @@ def test_redis_from_pool(request, from_url):
 
     assert called == 1
     pool.disconnect()
+
+
+@pytest.mark.parametrize(
+    "conn, error, expected_message",
+    [
+        (SSLConnection(), OSError(), "Error connecting to localhost:6379."),
+        (SSLConnection(), OSError(12), "Error 12 connecting to localhost:6379."),
+        (
+            SSLConnection(),
+            OSError(12, "Some Error"),
+            "Error 12 connecting to localhost:6379. Some Error.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/redis.sock"),
+            OSError(),
+            "Error connecting to unix:///tmp/redis.sock.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/redis.sock"),
+            OSError(12),
+            "Error 12 connecting to unix:///tmp/redis.sock.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/redis.sock"),
+            OSError(12, "Some Error"),
+            "Error 12 connecting to unix:///tmp/redis.sock. Some Error.",
+        ),
+    ],
+)
+def test_format_error_message(conn, error, expected_message):
+    """Test that the _error_message function formats errors correctly"""
+    error_message = conn._error_message(error)
+    assert error_message == expected_message
+
+
+def test_network_connection_failure():
+    with pytest.raises(ConnectionError) as e:
+        redis = Redis(port=9999)
+        redis.set("a", "b")
+    assert str(e.value) == "Error 111 connecting to localhost:9999. Connection refused."
+
+
+def test_unix_socket_connection_failure():
+    with pytest.raises(ConnectionError) as e:
+        redis = Redis(unix_socket_path="unix:///tmp/a.sock")
+        redis.set("a", "b")
+    assert (
+        str(e.value)
+        == "Error 2 connecting to unix:///tmp/a.sock. No such file or directory."
+    )

valkey-io/valkey-py

Co-authored-by: Salvatore Mesoraca <salvatore.mesoraca@aiven.io>
Signed-off-by: Salvatore Mesoraca <salvatore.mesoraca@aiven.io>

tests/test_asyncio/test_connection.py
@@ -14,7 +14,11 @@ from valkey._parsers import (
     parse_url,
 )
 from valkey.asyncio import ConnectionPool, Valkey
-from valkey.asyncio.connection import Connection, UnixDomainSocketConnection
+from valkey.asyncio.connection import (
+    Connection,
+    SSLConnection,
+    UnixDomainSocketConnection,
+)
 from valkey.asyncio.retry import Retry
 from valkey.backoff import NoBackoff
 from valkey.exceptions import ConnectionError, InvalidResponse, TimeoutError
@@ -497,18 +501,50 @@ async def test_connection_garbage_collection(request):
 
 
 @pytest.mark.parametrize(
-    "error, expected_message",
+    "conn, error, expected_message",
     [
-        (OSError(), "Error connecting to localhost:6379. Connection reset by peer"),
-        (OSError(12), "Error connecting to localhost:6379. 12."),
+        (SSLConnection(), OSError(), "Error connecting to localhost:6379."),
+        (SSLConnection(), OSError(12), "Error 12 connecting to localhost:6379."),
         (
+            SSLConnection(),
             OSError(12, "Some Error"),
-            "Error 12 connecting to localhost:6379. [Errno 12] Some Error.",
+            "Error 12 connecting to localhost:6379. Some Error.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/valkey.sock"),
+            OSError(),
+            "Error connecting to unix:///tmp/valkey.sock.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/valkey.sock"),
+            OSError(12),
+            "Error 12 connecting to unix:///tmp/valkey.sock.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/valkey.sock"),
+            OSError(12, "Some Error"),
+            "Error 12 connecting to unix:///tmp/valkey.sock. Some Error.",
         ),
     ],
 )
-async def test_connect_error_message(error, expected_message):
+async def test_format_error_message(conn, error, expected_message):
     """Test that the _error_message function formats errors correctly"""
-    conn = Connection()
     error_message = conn._error_message(error)
     assert error_message == expected_message
+
+
+async def test_network_connection_failure():
+    with pytest.raises(ConnectionError) as e:
+        valkey = Valkey(host="127.0.0.1", port=9999)
+        await valkey.set("a", "b")
+    assert str(e.value).startswith("Error 111 connecting to 127.0.0.1:9999. Connect")
+
+
+async def test_unix_socket_connection_failure():
+    with pytest.raises(ConnectionError) as e:
+        valkey = Valkey(unix_socket_path="unix:///tmp/a.sock")
+        await valkey.set("a", "b")
+    assert (
+        str(e.value)
+        == "Error 2 connecting to unix:///tmp/a.sock. No such file or directory."
+    )

tests/test_connection.py
@@ -291,3 +291,53 @@ def test_valkey_from_pool(request, from_url):
 
     assert called == 1
     pool.disconnect()
+
+
+@pytest.mark.parametrize(
+    "conn, error, expected_message",
+    [
+        (SSLConnection(), OSError(), "Error connecting to localhost:6379."),
+        (SSLConnection(), OSError(12), "Error 12 connecting to localhost:6379."),
+        (
+            SSLConnection(),
+            OSError(12, "Some Error"),
+            "Error 12 connecting to localhost:6379. Some Error.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/valkey.sock"),
+            OSError(),
+            "Error connecting to unix:///tmp/valkey.sock.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/valkey.sock"),
+            OSError(12),
+            "Error 12 connecting to unix:///tmp/valkey.sock.",
+        ),
+        (
+            UnixDomainSocketConnection(path="unix:///tmp/valkey.sock"),
+            OSError(12, "Some Error"),
+            "Error 12 connecting to unix:///tmp/valkey.sock. Some Error.",
+        ),
+    ],
+)
+def test_format_error_message(conn, error, expected_message):
+    """Test that the _error_message function formats errors correctly"""
+    error_message = conn._error_message(error)
+    assert error_message == expected_message
+
+
+def test_network_connection_failure():
+    with pytest.raises(ConnectionError) as e:
+        valkey = Valkey(port=9999)
+        valkey.set("a", "b")
+    assert str(e.value) == "Error 111 connecting to localhost:9999. Connection refused."
+
+
+def test_unix_socket_connection_failure():
+    with pytest.raises(ConnectionError) as e:
+        valkey = Valkey(unix_socket_path="unix:///tmp/a.sock")
+        valkey.set("a", "b")
+    assert (
+        str(e.value)
+        == "Error 2 connecting to unix:///tmp/a.sock. No such file or directory."
+    )