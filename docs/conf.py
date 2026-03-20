4ef87882574e90942782e20cda85c0293df73a4e
Kurt McKee <contactme@kurtmckee.org>
2026-03-20 06:26:01 -0500

    Docs: Fix the valkey logo path
    Signed-off-by: Kurt McKee <contactme@kurtmckee.org>

--- docs/conf.py
+++ docs/conf.py
@@ -157,7 +157,7 @@ html_theme_options = {
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = "_static/valkey-cube-red-white-rgb.svg"
+html_logo = "_static/Valkey-logo.svg"
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
